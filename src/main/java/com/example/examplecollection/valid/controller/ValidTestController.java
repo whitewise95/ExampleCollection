5f246217178ef91727ae428d70c782aea2b35d0a
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-05-11 22:12:48 +0900

    자바 ORM 표준 JPA 프로그래밍 책읽으면 서 테스트 셋팅

--- src/main/java/com/example/examplecollection/valid/controller/ValidTestController.java
+++ src/main/java/com/example/examplecollection/valid/controller/ValidTestController.java
@@ -11,42 +11,42 @@ import javax.validation.Valid;
 @RequestMapping("/v1")
 public class ValidTestController {
 
-    /**
-     * @Valid를 이용한 검증 예시
-     * 이메일: 이메일 형식으로 입력을 받아야 한다.
-     * 비밀번호: 빈 값을 받으면 안된다.
-     * 이름: 사용자의 이름은 null이면 안된다.
-     * 나이: 사용자의 나이는 19세 이상이여야 한다.
-     */
-    @PostMapping("/validTest")
-    public void validTest(@RequestBody @Valid UserRequestDto userRequestDto) {
-        System.out.println("로직 수행");
-        System.out.println(userRequestDto);
-    }
+	/**
+	 * @Valid를 이용한 검증 예시
+	 * 이메일: 이메일 형식으로 입력을 받아야 한다.
+	 * 비밀번호: 빈 값을 받으면 안된다.
+	 * 이름: 사용자의 이름은 null이면 안된다.
+	 * 나이: 사용자의 나이는 19세 이상이여야 한다.
+	 */
+	@PostMapping("/validTest")
+	public void validTest(@RequestBody @Valid UserRequestDto userRequestDto) {
+		System.out.println("로직 수행");
+		System.out.println(userRequestDto);
+	}
 
-    /**
-     * @Validated를 이용해 그룹화하여 검증
-     * 이메일: 이메일 형식으로 입력을 받아야 한다.
-     * 이름: 사용자의 이름은 null이면 안된다.
-     */
-    @PostMapping("/userUpdateTest")
-    public void UserNameUpdate(@RequestBody @Validated(UserValidGroup.UserUpdate.class) UserRequestDto userRequestDto) {
-        System.out.println("로직 수행 중");
-        System.out.println("변경 완료");
-    }
+	/**
+	 * @Validated를 이용해 그룹화하여 검증
+	 * 이메일: 이메일 형식으로 입력을 받아야 한다.
+	 * 이름: 사용자의 이름은 null이면 안된다.
+	 */
+	@PostMapping("/userUpdateTest")
+	public void UserNameUpdate(@RequestBody @Validated(UserValidGroup.UserUpdate.class) UserRequestDto userRequestDto) {
+		System.out.println("로직 수행 중");
+		System.out.println("변경 완료");
+	}
 
-    /**
-     * @Validated를 이용해 그룹화하여 검증
-     * 이메일: 이메일 형식으로 입력을 받아야 한다.
-     * 비밀번호: 빈 값을 받으면 안된다.
-     * 이름: 사용자의 이름은 null이면 안된다.
-     * 나이: 사용자의 나이는 19세 이상이여야 한다.
-     */
-    @PostMapping("/userCreatTest")
-    public void UserCreate(
-            @RequestBody
-            @Validated(UserValidGroup.UserCreate.class) UserRequestDto userRequestDto) {
-        System.out.println("로직 수행 중");
-        System.out.println("유저 생성");
-    }
+	/**
+	 * @Validated를 이용해 그룹화하여 검증
+	 * 이메일: 이메일 형식으로 입력을 받아야 한다.
+	 * 비밀번호: 빈 값을 받으면 안된다.
+	 * 이름: 사용자의 이름은 null이면 안된다.
+	 * 나이: 사용자의 나이는 19세 이상이여야 한다.
+	 */
+	@PostMapping("/userCreatTest")
+	public void UserCreate(
+		@RequestBody
+		@Validated(UserValidGroup.UserCreate.class) UserRequestDto userRequestDto) {
+		System.out.println("로직 수행 중");
+		System.out.println("유저 생성");
+	}
 }
